ba07b014fe629a5c3ada85e60c02de0bc8b7f8a3
Kevin <l8iunix8@gmail.com>
2021-04-01 00:15:19 +0800

    2021/04/01 add logger

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from lib.init import KYJStreamInit
 from lib.config import KYJStreamConfig
+from lib.log import KYJStreamLogger
 from framework_init import FrameWork
 
 if __name__ == "__main__":
     try:
         KYJStreamInit.init()
+        KYJStreamLogger.init()
         FrameWork.init()   
     except Exception as e:
-        print(e)
+       print(e) 
