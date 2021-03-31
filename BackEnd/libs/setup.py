2d3d0ac095870bdf429c02e2bee0bf65b9b4ceaf
Kevin <l8iunix8@gmail.com>
2021-03-31 19:47:12 +0800

    2021/03/31 backend init

---
+++ BackEnd/libs/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname('./'))
+with open('libs/README.md', 'rt') as f:
+    long_description = f.read()
+
+setup(
+    name="KYJStream",
+    version="0.1.0",
+    author="KevinSyu & Dr.Chou",
+    author_email="l8iunix8@gmail.com,callmeccy@gmail.com",
+    description="Our side project",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="",
+    packages=['libs/lib']
+)
---
+++ BackEnd/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from lib.init import KYJStreamInit
+from lib.config import KYJStreamConfig
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Flask Dockerized TEST'
+
+
+if __name__ == "__main__":
+    KYJStreamInit.init()
+    KYJStreamConfig.init()
+    # app.run(debug=True, host='0.0.0.0', port=8888)
