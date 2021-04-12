228a86f4d68997df98aab84479e3934079462de0
Kevin <l8iunix8@gmail.com>
2021-04-12 21:13:42 +0800

    2021/04/12 test AES

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -5,13 +5,27 @@ from lib.log import KYJStreamLogger
 from lib.db_manager import DBManager
 from framework_init import FrameWork
 from lib.log import KYJStreamLogger
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+import base64 
+
+
 if __name__ == "__main__":
     try:
         KYJStreamInit.init()
         KYJStreamLogger.init()
         DBManager.init()
+        key = bytes("IXqAiIIVkcD8t70E","ascii")
+        cipher = AES.new(key,AES.MODE_ECB)
+        s = cipher.encrypt(bytes("1234567891234567","ascii"))
         
-        FrameWork.init()        
+        print(base64.encodebytes(s))
+        print(base64.encodebytes(key))
+
+
+        # FrameWork.init()
+
+
     except Exception as e:
         KYJStreamLogger.log_error(e)
         print(e) 
