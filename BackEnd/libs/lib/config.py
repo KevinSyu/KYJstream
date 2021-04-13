1d55c740cc521d9d815c299f255bceb30216614c
Kevin <l8iunix8@gmail.com>
2021-04-13 23:09:19 +0800

    2021/04/13 modify config

--- BackEnd/libs/lib/config.py
+++ BackEnd/libs/lib/config.py
@@ -1,17 +1,24 @@
 import configparser
 import lib.exception.config_not_found_exception
-
+import os
 
 class KYJStreamConfig:
 
     __config = configparser.ConfigParser()
-    __crypt_config_section = ''
-    __crypt_config_key = ''
 
     @staticmethod
     def init():
+        
         KYJStreamConfig.__config.read('./etc/default.ini')
         KYJStreamConfig.__config.read('./etc/common.ini')
+
+        if os.environ.get('ENVIRONMENT') and os.environ.get('ENVIRONMENT') == "dockerdev":
+            KYJStreamConfig.set_db_config_section('kyjstream.dockerdev.db.mysql.config')
+            KYJStreamConfig.set_host('0.0.0.0')
+        else:
+            KYJStreamConfig.set_db_config_section('kyjstream.db.mysql.config')
+            KYJStreamConfig.set_host('127.0.0.1')
+
         KYJStreamConfig.set_crypt_section('kyjstream.secret.crypt')
         KYJStreamConfig.set_crypt_key('KEY')
         KYJStreamConfig.set_config(KYJStreamConfig.get_crypt_section(),KYJStreamConfig.get_crypt_key(),"IXqAiIIVkcD8t70E")
@@ -44,10 +51,25 @@ class KYJStreamConfig:
         return KYJStreamConfig.__crypt_config_key
 
     @staticmethod
+    def get_db_config_section():
+        return KYJStreamConfig.__db_config_section
+
+    @staticmethod
+    def get_host():
+        return KYJStreamConfig.__host
+
+    @staticmethod
     def set_crypt_section(section):
         KYJStreamConfig.__crypt_config_section = section
 
     @staticmethod
     def set_crypt_key(key):
         KYJStreamConfig.__crypt_config_key = key
-    
+
+    @staticmethod
+    def set_db_config_section(section):
+        KYJStreamConfig.__db_config_section = section
+
+    @staticmethod
+    def set_host(host):
+        KYJStreamConfig.__host = host
--- BackEnd/libs/lib/db_manager.py
+++ BackEnd/libs/lib/db_manager.py
@@ -10,13 +10,8 @@ class DBManager:
   
   @staticmethod
   def init():
-    
-    if os.environ['ENVIRONMENT'] == "dockerdev":
-      DBManager.__db_config_section = 'kyjstream.dockerdev.db.mysql.config'
-    else:
-      DBManager.__db_config_section = 'kyjstream.db.mysql.config'
 
-    DBManager.__db = DataBase(DBManager.__db_config_section)
+    DBManager.__db = DataBase(KYJStreamConfig.get_db_config_section())
 
   @staticmethod
   def get_db():
