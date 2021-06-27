3947746c5d3041532acea54fe530aa8c253c72b6
johnnychen <jonny50304@gmail.com>
2021-06-27 23:52:02 +0800

    FrontEnd: create errlog-page，register-page，search-page，shared-material， BackEnd: update CORS(app)

--- BackEnd/framework_init.py
+++ BackEnd/framework_init.py
@@ -1,6 +1,7 @@
 from flask import Flask,Blueprint
 from lib.config import KYJStreamConfig
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 import os
 import datetime
 
@@ -20,6 +21,7 @@ class FrameWork:
   def init():
     jwt = JWTManager()
     app = Flask(__name__)
+    CORS(app)
     app.config['JWT_SECRET_KEY'] = KYJStreamConfig.get_str(FrameWork.jwt_config_section,'SECRET_KEY')
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(minutes=KYJStreamConfig.get_int(FrameWork.jwt_config_section,'ACCESS_TOKEN_EXPIRES_TIME_IN_MINUTES'))
     app.config['JWT_REFRESH_TOKEN_EXPIRES'] = datetime.timedelta(days=KYJStreamConfig.get_int(FrameWork.jwt_config_section,'REFRESH_TOKEN_EXPIRES_TIME_IN_DAYS'))
